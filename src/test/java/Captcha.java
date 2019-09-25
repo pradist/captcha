d7f06ca78f9914fb06eed8a5eca95e1bc9c01f62
Pradist <pradist.ke@gmail.com>
2019-09-25 15:10:10 +0700

    Init

---
+++ src/test/java/Captcha.java
@@ -0,0 +1,12 @@
+public class Captcha {
+
+    private final int leftOperand;
+
+    public Captcha(int i, int i1, int i2, int i3) {
+        leftOperand = i1;
+    }
+
+    public String getLeftOperand() {
+        return leftOperand+"";
+    }
+}
